17a41a1ac0cd265881ba471284f39509d332d318
matthewtoghill <matthew.toghill@outlook.com>
2021-12-23 23:10:05 +0000

    Day 16 part 1 completed

---
+++ Day16/Packet.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day16
+{
+    public class Packet
+    {
+        public int Version { get; set; }
+        public int TypeID { get; set; }
+        public long Value { get; set; }
+        public List<Packet> SubPackets { get; set; } = new List<Packet>();
+        public int BitsRead { get; set; }
+
+        public Packet(string packetBinary)
+        {
+            Version = BinaryToInt(packetBinary.Substring(0, 3));
+            TypeID = BinaryToInt(packetBinary.Substring(3, 3));
+            BitsRead = 6;
+
+            if (TypeID == 4)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = BitsRead; i < packetBinary.Length; i += 5)
+                { 
+                    sb.Append(packetBinary.Substring(i + 1, 4));
+                    BitsRead += 5;
+                    if (packetBinary[i] == '0') break;
+                }
+
+                Value = BinaryToLong(sb.ToString());
+            }
+            else
+            {
+                if (packetBinary.Substring(BitsRead++, 1) == "0")
+                {
+                    int subPacketsLength = BinaryToInt(packetBinary.Substring(BitsRead, 15));
+                    BitsRead += 15;
+                    int subBitsRead = 0;
+                    while (subBitsRead < subPacketsLength)
+                    {
+                        Packet sub = new Packet(packetBinary.Substring(BitsRead, subPacketsLength - subBitsRead));
+                        subBitsRead += sub.BitsRead;
+                        BitsRead += sub.BitsRead;
+                        SubPackets.Add(sub);
+                    }
+                }
+                else
+                {
+                    int subPacketsCount = BinaryToInt(packetBinary.Substring(BitsRead, 11)); ;
+                    BitsRead += 11;
+                    while (SubPackets.Count < subPacketsCount)
+                    {
+                        Packet sub = new Packet(packetBinary.Substring(BitsRead));
+                        BitsRead += sub.BitsRead;
+                        SubPackets.Add(sub);
+                    }
+                }
+            }
+        }
+
+        private static long BinaryToLong(string binary) => Convert.ToInt64(binary.ToString(), 2);
+        private static int BinaryToInt(string binary) => Convert.ToInt32(binary.ToString(), 2);
+    }
+}
---
+++ Day16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Day16
+{
+    public class Program
+    {
+        private static readonly string input = File.ReadAllText(@"..\..\..\data\day16.txt");
+        static void Main()
+        {
+            string binary = HexToBinary(input);
+            Packet packet = new Packet(binary);
+
+            Console.WriteLine($"Part 1: {GetVersionSum(packet)}");
+        }
+
+        private static int GetVersionSum(Packet packet)
+        {
+            int sum = packet.Version;
+            foreach (Packet subpacket in packet.SubPackets)
+                sum += GetVersionSum(subpacket);
+            
+            return sum;
+        }
+
+
+        #region Helpers
+        private static string HexToBinary(string hex)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in hex)
+                sb.Append(CharToBinary(c));
+            
+            return sb.ToString();
+        }
+
+        private static string CharToBinary(char c) => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0');
+        #endregion
+    }
+}
