1fbdc476b42579cf028c4fb15d1d83dd9cae420c
matthewtoghill <matthew.toghill@outlook.com>
2021-12-29 16:41:05 +0000

    Day 17 completed

---
+++ Day17/Probe.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Day17
+{
+    public class Probe
+    {
+        public (int X, int Y) StartVelocity;
+        public (int X, int Y) Velocity;
+        public (int X, int Y) Position;
+        public (int minX, int maxX, int minY, int maxY) Target;
+        public List<(int X, int Y)> Steps = new List<(int X, int Y)>();
+        public bool Success { get; private set; }
+
+        public Probe((int, int) startVelocity, (int,int,int,int) target)
+        {
+            StartVelocity = startVelocity;
+            Velocity = startVelocity;
+            Target = target;
+            Position = (0, 0);
+            Success = SimulateSteps();
+        }
+
+        public bool IsInTarget => Position.X.IsBetween(Target.minX, Target.maxX) && Position.Y.IsBetween(Target.minY, Target.maxY);
+        public bool HasOvershotTarget => Position.X > Target.maxX || Position.Y < Target.minY;
+
+        private bool SimulateSteps()
+        {
+            while (!HasOvershotTarget)
+            {
+                Step();
+                Steps.Add(Position);
+                if (IsInTarget) return true;
+            }
+
+            return false;
+        }
+
+        private void Step()
+        {
+            Position.X += Velocity.X;  // Increase X position by Velocity X
+            Position.Y += Velocity.Y;  // Increase Y position by Velocity Y
+
+            // Adjust Velocity X towards 0 by 1
+            if (Velocity.X > 0) Velocity.X--;
+            else if (Velocity.X < 0) Velocity.X++;
+
+            Velocity.Y--;   // Decrease Velocity Y by 1
+        }
+    }
+}
---
+++ Day17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day17
+{
+    public class Program
+    {
+        public static readonly (int minX, int maxX, int minY, int maxY) targetRange = (57, 116, -198, -148);
+        static void Main()
+        {
+            List<Probe> probes = new List<Probe>();
+            for (int x = 0; x <= targetRange.maxX; x++)
+            {
+                for (int y = Math.Abs(targetRange.minY); y >= targetRange.minY; y--)
+                {
+                    Probe p = new Probe((x, y), targetRange);
+                    if (p.Success) probes.Add(p);
+                }
+            }
+
+            int maxHeight = probes.Select(p => p.Steps.Max(s => s.Y)).Max();
+            Console.WriteLine($"Part 1: {maxHeight}");
+            Console.WriteLine($"Part 2: {probes.Count}");
+        }
+    }
+
+    public static class Helpers
+    {
+        public static bool IsBetween<T>(this T value, T min, T max) where T : IComparable<T>
+        {
+            return min.CompareTo(value) <= 0 && value.CompareTo(max) <= 0;
+        }
+    }
+}
